3ecf836a362e931f05c1c703f44ffb3e9a14b278
Ruslan Galimullin <ruslan.gali.galimullin@yandex.ru>
2021-08-05 00:50:33 +0300

    lesson03

--- scripts2.js
+++ scripts2.js
@@ -1,3 +1,5 @@
+////////////////////////////////////////////////////////////////////////////////////////////////  lesson02
+
 number=266219;
      result = 1;
     while (number) {
@@ -6,7 +8,6 @@ number=266219;
     }
     console.log(result);
 
-    //
 
 newResult=1
     for(i=1;i<=3;i++){
@@ -14,7 +15,6 @@ newResult=1
     }
     console.log(newResult);
 
-    //
 
    x= newResult.toString()
    newResult=x.split('')
@@ -23,3 +23,49 @@ newResult=1
 
 
     console.log(result)
+
+////////////////////////////////////////////////////////////////////////////////////////////////  lesson03
+
+
+    let language_1=function(lang){
+
+    if (lang=='ru'){
+        return('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье')
+    }
+    else if (lang=='en'){
+        return('monday, tuestay, wednesday, thursday, friday, saturday, sunday')
+    }
+}
+console.log(language_1('ru'))
+
+//
+let language_2=function(lang){
+    switch(lang){
+    case 'ru':
+        return('понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+    case 'en':
+        return('monday, tuestay, wednesday, thursday, friday, saturday, sunday');
+    }
+
+}
+console.log(language_2('ru'))
+
+//
+let language_3=function(lang){
+
+let arr= {
+    ru: ['понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'],
+    en: ['monday, tuesday, wednesday, thursday, friday, saturday, sunday']
+}
+return arr[lang]
+}
+console.log(language_3('en'))
+
+//
+
+let post=function(namePerson){
+    (namePerson=='Артем') ? console.log('Директор') : console.log('Студент');
+    (namePerson=='Максим') ? console.log('Преподаватель') : console.log('Студент')
+
+}
+post('Максим')
